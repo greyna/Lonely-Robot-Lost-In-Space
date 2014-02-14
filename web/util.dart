3f4a520a76543921e6ad0cd2016dde7f4e4bd71b
greyna <vandekeu@gmail.com>
2014-02-14 22:17:51 +0100

    Licence added v0.1.2

--- web/util.dart
+++ web/util.dart
@@ -1,3 +1,24 @@
+/*
+    This file is part of actionman.
+
+    actionman is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    actionman is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with actionman.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * util.dart v0.1.2
+ * 
+ * Code hosted at github.com/greyna/actionman.dart. Made by greyna. Under GPL licence.
+ */
 part of actionman;
 
 class ActionOverTime {
